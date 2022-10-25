db709436d94372e4f3b2c30ce8c2c4259667c80e
arav1nd <aravindkayal@constellation4.com>
2022-10-24 22:25:31 -0500

    done

--- React/src/info/info.js
+++ React/src/info/info.js
@@ -23,8 +23,7 @@ function HeaderInfo() {
                 <div className="d-flex flex-direction-row justify-around info-link">
                     <img src={pickupDelivery} alt='pickup and delivery'/>
                     <p>
-                        <span className="font-oswald text-header">
-                            And the cherry on top </span><br/>
+                        <span className="font-oswald text-header"> And the cherry on top </span><br/>
                         <span className="about-description"> You can view posts, products and advertisements with one click </span>
                         <Link className="services-link" to="/services">Explore our Services <img className="arrow-right" alt='right arrow'
                             src={arrowRight} /></Link>
